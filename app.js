d5ea47d73cadde0e8672f87ef6b07db416fd428b
microbet <34072415+microbet@users.noreply.github.com>
2018-11-13 22:17:05 -0800

    done for the night

--- app.js
+++ app.js
@@ -59,6 +59,7 @@ app.post('/api/posts', verifyToken, (req, res) => {  // this is a mock of a real
 
 // I think I need to not use multer here. and write my own middleware https://expressjs.com/en/guide/using-middleware.html
 // or I just have to handle the upload inside the function in the argument list of the post function.
+// this may have to check the file size
 var storage = multer.diskStorage({
 	destination: function (req, file, cb) {
 		cb(null, '../solarreact/public/img')
@@ -67,23 +68,44 @@ var storage = multer.diskStorage({
 		fs.readFile('../solarreact/src/ImageData.json', 'utf8', (err, data) => {
 			if (err) throw err;
 			imgDataArr = JSON.parse(data);
-		//	const filename = imgDataArr[imgDataArr.length-1][2].toString() + '_' + imgDataArr[imgDataArr.length-1][0].toString() + '.jpg';
 			const filename = imgDataArr[0] ? 
 								imgDataArr[0][0].toString() + '_' + imgDataArr[imgDataArr.length-1][0].toString() + '.jpg'
 							 :
 								'1.jpg';
-			const parentId = imgDataArr[0] ? imgDataArr[0][0] : 0;
-			imgDataArr.push([imgDataArr.length+1, "./img/" + filename, parentId, "this would be the caption here"]);
-			console.log(JSON.stringify(imgDataArr));
-			writeImageData(imgDataArr);
+			var allowedMimes = ['image/jpeg', 'image/pjpeg', 'image/png', 'image/gif']; 
+			if (allowedMimes.includes(file.mimetype) && file.size < (1024*1024)) {
+				const parentId = imgDataArr[0] ? imgDataArr[0][0] : 0;
+				imgDataArr.push([imgDataArr.length+1, "./img/" + filename, parentId, "this would be the caption here"]);
+				console.log(JSON.stringify(imgDataArr));
+				writeImageData(imgDataArr);
+			}
 			cb(null, filename);
 		});
 	}
 
 });
 
+var limits = {
+	files: 1,
+	fileSize: 1024 * 1024, // 1MB max for now anyway
+};
+
+var fileFilter = function(req, file, cb) {
+	// supported image file mimetypes
+	var allowedMimes = ['image/jpeg', 'image/pjpeg', 'image/png', 'image/gif']; 
+	if (allowedMimes.includes(file.mimetype)) {
+		// allow supported image files
+		cb(null, true);
+	} else {
+		// invalid file type
+		cb(new Error('Invalid file type.  Only jpg, png and gif image files are allowed.'));
+	}
+};
+
 var upload = multer({ 
-	storage: storage
+	storage: storage,
+	limits: limits,
+	fileFilter: fileFilter
 });
 app.post('/api/imgupload', upload.single('image'), function(req, res, next) { // upload pic to image directory
 	console.log("do I get anywhere");
